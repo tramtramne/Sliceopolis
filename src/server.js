56b0e926765d14c98a8d24d9a7da92072c9ecb1b
Ngoc Tram <ntram2k3@gmail.com>
2023-11-03 13:39:18 +0700

    add file prettierrc

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
-const app = require("./app")
+const app = require('./app');
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('<h4>Hello world</h4>');
+});
+
 app.listen(port, () => {
-	console.log(`Listening on port ${port}`)
-})
+    console.log(`Listening on port ${port}`);
+});
