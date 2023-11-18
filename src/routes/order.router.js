b512452977df9d23483ca376a1d919db10d500ea
Ha Phan Hai <ha.phanhai4869@hcmut.edu.vn>
2023-11-18 23:46:21 +0700

    feat(order): get all orders and get order by id

--- src/routes/order.router.js
+++ src/routes/order.router.js
@@ -1,6 +1,8 @@
-const { createOrder } = require('../controllers/order.controller');
+const { createOrder, getAllOrder, getOrderById } = require('../controllers/order.controller');
 const router = require('express').Router();
 
 router.post('/', createOrder);
+router.get('/', getAllOrder);
+router.get('/:id', getOrderById); 
 
 module.exports = router;
